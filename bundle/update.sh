2fb2e1b806dcbb6e7954828b43d329f5aad02922
Paulo Santos <psantos@q-better.com>
2017-01-12 00:12:58 +0000

    Update packages

---
+++ bundle/update.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail -o errtrace -x
+IFS=$'\n\t'
+function recompile_cmdt()
+{
+    cd Command-T
+    rake make
+    cd ..
+}
+function update_repos()
+{
+    for dir in $(ls -d */); do
+        cd $dir
+        git pull --rebase
+        cd ..
+    done
+}
+function main()
+{
+    update_repos
+    recompile_cmdt
+}
+main
